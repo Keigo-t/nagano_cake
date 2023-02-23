c184b824a8cb104e5ee3f714922e06453529c852
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2023-02-23 13:52:13 +0000

    [fix]admin/items_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     get 'top' => 'homes#top'
+    resources :items, only: [:new, :index, :show, :create, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

61043b103f81c6f6f90728b422005bd31c01ced8
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2023-02-25 13:04:59 +0000

    [fix]associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :cart_items, dependent: :withdraw
-  has_many :orders, dependent: :withdraw
-  has_many :addresses, dependent: :withdraw
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 
 end
